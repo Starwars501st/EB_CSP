c19a6662771a10e55d7e071b81634c9e3e4bedd9
Starwars501st <eva.briggs@ucas-edu.net>
2025-09-08 13:45:16 -0600

    I changed it to a print statment

--- Notes/String_notes.py
+++ Notes/String_notes.py
@@ -1,1 +1,15 @@
-I did it!
+# EB 6th String notes
+
+#print( " I did it! ")
+
+#Examples of string 
+first_name = input("What is your last name:\n").strip().upper().lower().capitalize().title()
+
+last_name = input("What is your first name:\n").strip().upper()
+
+full_name = first_name + " " + last_name
+
+sentence = "A quick brown fox jumps over the lazy dog"
+
+print("Welcome to my program", full_name + "!")
+
