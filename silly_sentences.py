ac2f793fe998f1c40a543a46dde582eb5f3684a9
Starwars501st <eva.briggs@ucas-edu.net>
2025-09-10 17:39:16 -0600

    I finished Silly Sentences

--- silly_sentences.py
+++ silly_sentences.py
@@ -1,2 +1,7 @@
 # EB 6th Silly Sentences
 
+animal=input("Enter an animal ")
+place=input("Enter an place ")
+food=input("Enter an food ")
+
+print("One day, a " + animal + " walked into a "+  place + " and ordered " + food + "!" )
