dfdeec12e0589dd2e2b9daa5fcb18dbba483ba85
Starwars501st <eva.briggs@ucas-edu.net>
2025-09-15 13:43:52 -0600

    I did functions notes

---
+++ Notes/functions_notes.py
@@ -0,0 +1,16 @@
+# EB 6th Functions Notes
+
+def welcome():
+     
+     name = input("What is your name?")
+    print(f"Hello {name}!")
+ 
+print(f"The function is over!")
+
+welcome()
+
+def add():
+    print(num_one+num_two)
+
+num_one=12
+num_two=14
