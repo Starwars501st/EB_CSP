dd50e925866c4cd58dd97d504c4c1f0526a426ef
Starwars501st <eva.briggs@ucas-edu.net>
2025-09-15 22:15:20 -0600

    I did updated hello world

---
+++ update_hello_world.py
@@ -0,0 +1,11 @@
+# EB 6th Update Hello World - python 
+
+def say_hello(name):
+    print("Hello, " + name + "!")
+
+say_hello("Amy")
+say_hello("Carlos")
+say_hello("John")
+say_hello("Eva")
+say_hello("Liam")
+say_hello("Emma")
