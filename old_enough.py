0e6526115a448f389c3896f2c46ccdb30cfd4c05
Starwars501st <eva.briggs@ucas-edu.net>
2025-09-18 13:44:09 -0600

    I am working on old enought assignment

---
+++ old_enough.py
@@ -0,0 +1,9 @@
+# EB 6th Old Enough 
+
+age = input("You are old enough for school")
+if age = 4 
+age = input("You are old enough to vote")
+if age = 18
+age = input("You are old enough to drive")
+if age = 16
+age = input("You are old enough to get a learners permit")
