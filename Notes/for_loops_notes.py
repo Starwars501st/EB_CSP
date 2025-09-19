c3fc1f3192f43a57bc8d424fc90c4d7b899606aa
Starwars501st <eva.briggs@ucas-edu.net>
2025-09-19 13:13:07 -0600

    I am doing loops notes

---
+++ Notes/for_loops_notes.py
@@ -0,0 +1,11 @@
+# EB 6th Time and For loops notes
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+#print(f"The time in seconds since Jan 1, 1970")
+print(f"The time is :{current}")
+print(f"The hour is:{hour}") 
+
