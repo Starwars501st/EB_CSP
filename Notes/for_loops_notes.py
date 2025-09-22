92a97a295b42df1b49791edf0b42dba3c1a27969
Starwars501st <eva.briggs@ucas-edu.net>
2025-09-22 13:41:47 -0600

    Loops notes

--- Notes/for_loops_notes.py
+++ Notes/for_loops_notes.py
@@ -11,3 +11,45 @@ print(f"The hour is:{hour}")
 
 #Lists
 siblings = ["Lissie", "Oliver"]
+
+#Starts at zero 0 not 1
+print(siblings[1])
+
+#siblings[2] = "Lissie"
+#siblings[1] = "Oliver"
+
+#For loop
+for siblings in siblings:
+    print(f"Hello {siblings}")
+
+    for x in range(20,-11,-1):
+        print(x)
+
+#while True:
+#  print("Oh NO!")
+
+while i <21:
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+    i += 1
+
+    import random
+    number = random.randint(1,20)
+    x = 1
+    """while x !=number:
+        print("Duck")
+        x =+ 1
+
+
+print("Goose!")"""
+
+while True:
+    if number == x:
+        print("Goose!")
+        break
+    else:
+        print("Duck!")
+        x += 1
+
