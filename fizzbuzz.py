5c1a120cff2d8ff7f594144bf94c825530c9d5e4
Starwars501st <eva.briggs@ucas-edu.net>
2025-09-23 13:43:01 -0600

    fizz buzz assignment

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+# EB 6th FizzBuzz
+
+i = 0
+while i <21:
+
+    if i % 2 == 0:
+    
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+    i += 3
+
+    while x !=number:
+        print("Fizz")
+        x =+ 1
+
+    while x !=number:
+        print("Buzz")  
+
