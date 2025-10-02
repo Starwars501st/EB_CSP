ac7496273ebbae48165a3b3f8485681c4b876aa6
Starwars501st <eva.briggs@ucas-edu.net>
2025-10-02 13:44:17 -0600

    IDK

---
+++ C_programming/notes/variables_notes.c
@@ -0,0 +1,14 @@
+// EB 6th Variables Notes C
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi =3.14;
+    char grade = 'A'; //will only hold 1 letter
+    char name[]= "Eva";
+    //bool passing = true
+
+    printf("%d", num);
+
+    return 0;
+}
