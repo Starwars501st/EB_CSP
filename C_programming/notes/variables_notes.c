8cba3859a004c5da6aeab5b1a8c75c0b80ce8d78
Starwars501st <eva.briggs@ucas-edu.net>
2025-10-03 13:44:09 -0600

    this is variables notes

--- C_programming/notes/variables_notes.c
+++ C_programming/notes/variables_notes.c
@@ -7,8 +7,44 @@ int main(void){
     char grade = 'A'; //will only hold 1 letter
     char name[]= "Eva";
     //bool passing = true
-
-    printf("%d", num);
+  printf("%d", num);
 
     return 0;
 }
+//int = 4 bytes
+// float = 4 bytes
+// double = 8 bytes
+// char = 1 bytes
+// char = list of characters for a string
+/*
+char grade; will hold one letter
+bool passing = true
+*/
+ #include <stdio.h>
+ 
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade; //Will only hold 1 letter
+    char name[20]= "Eva";
+    //bool passing = true;
+    printf("What is your letter grade: ");
+    scanf("%c", &grade);
+    // first info your getting is what is the info and the second one is what to do with it.
+    printf("Tell me a number: ");
+    scanf("%c", &num);
+    // the next comment lets you use a space;
+    printf("tell me your name:");
+    fgets(name, sizeof(name), stdin);
+    printf("%d\n", num);
+    printf("%s has a %c grade in the class!\n", name, grade);
+    return 0;
+}
+
+
+/*
+
+
+
+*/
+
