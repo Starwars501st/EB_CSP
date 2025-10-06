c2e1f142b23f63f0202347c8ca32495c0805eb0d
Starwars501st <eva.briggs@ucas-edu.net>
2025-10-06 13:44:02 -0600

    Expression notes

---
+++ C_programming/notes/expressions_notes.c
@@ -0,0 +1,14 @@
+// EB 6th Expressions Notes
+#include <stdio.h>
+
+int main(void){
+    int year = 2025; //whole numbers
+    float pi = 3.14; //decimals 
+    double long_pi = 3.14159265359; //decimals that are 2x as long
+
+    printf("%d", 8/3); //cast is specificly stating the data type
+    printf("%f\n", 8/3.0);
+
+    return 0;
+
+}
