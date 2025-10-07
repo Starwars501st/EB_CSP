d16f8f5bc9103424dd55b2a39bb47bde0f4dfb6c
Starwars501st <eva.briggs@ucas-edu.net>
2025-10-06 18:27:14 -0600

    I finished Variables practice

---
+++ C_programming/notes/practices/variables_practice.c
@@ -0,0 +1,33 @@
+// EB 6th Variables Practice 
+
+#include <stdio.h>
+
+int main() {
+   
+    char name[] = "Eva Briggs";
+    char breakfast[] = "Waffles and fruit";
+    char favoriteColor[] = "Blue";
+    char schoolName[] = "U.C.A.S.";
+    char eyeColor[] = "Gray";
+    char favoriteSubject[] = "Science";
+
+    
+    int luckyNumber = 5;
+    int currentYear = 2025;
+    int age = 14;
+    float gpa = 3.75;
+
+    printf("Name: %s\n", name);
+    printf("Lucky Number (1-10): %d\n", luckyNumber);
+    printf("GPA: %.2f\n", gpa);
+    printf("Breakfast: %s\n", breakfast);
+    printf("Favorite Color: %s\n", favoriteColor);
+    printf("School Name: %s\n", schoolName);
+    printf("Current Year: %d\n", currentYear);
+    printf("Eye Color: %s\n", eyeColor);
+    printf("Age: %d\n", age);
+    printf("Favorite Subject: %s\n", favoriteSubject);
+
+
+    return 0;
+}
