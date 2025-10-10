e99eaf0e05765054e53def168b1ceea4d0d3ffb3
Starwars501st <eva.briggs@ucas-edu.net>
2025-10-09 19:40:01 -0600

    Silly Sentences

--- C_programming/notes/practices/name_decorator.c
+++ C_programming/notes/practices/name_decorator.c
@@ -6,14 +6,18 @@ int main (void){
     char name [25];
 
     printf("What is your name?")
-    
+    char name[50];
+    char decoration[50];
+    char decorated_name[100];
+    fgets(name, sizeof(name), stdin);
 
+    name[strcspn(name, "\n")] = '\0';
+    printf("Enter a decoration <<<<<< >>>>>> ");
+    fgets(decoration, sizeof(decoration), stdin);
 
-
-
-
-
-
+    decoration[strcspn(decoration, "\n")] = '\0';
+    printf("<<<<< decorated_name >>>>>");
 
     return 0;
+
 }
---
+++ C_programming/notes/practices/silly_sentences.c
@@ -0,0 +1,22 @@
+// EB 6th Silly Sentences -C
+
+#include <stdio.h>
+
+int main() {
+    char animal[30];
+    char place[30];
+    char food[30];
+
+    printf("Enter an animal: ");
+    scanf("%29s", animal);
+
+    printf("Enter a place: ");
+    scanf("%29s", place);
+
+    printf("Enter a type of food: ");
+    scanf("%29s", food);
+
+    printf("One day, a %s went to %s to buy some %s.\n", animal, place, food);
+
+    return 0;
+}
