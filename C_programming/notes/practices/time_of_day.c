d9f3d02115e48983a013879b60fd6caec590a512
Starwars501st <eva.briggs@ucas-edu.net>
2025-10-13 16:42:32 -0600

    I finished time of day assignment

--- C_programming/notes/practices/time_of_day.c
+++ C_programming/notes/practices/time_of_day.c
@@ -3,10 +3,22 @@
 #include <stdio.h>
 
 int main(void){
-
     
+  int hour;
 
+    printf("What is the hour in military time (0-23)? ");
+    scanf("%d", &hour);
 
+    if (hour < 0 || hour > 23) {
+        printf("Invalid hour. Please enter a number between 0 and 23.\n");
+    } else if (hour < 12) {
+        printf("Good Morning!\n");
+    } else if (hour < 18) {
+        printf("Good Afternoon!\n");
+    } else {
+        printf("Good Evening!\n");
+
+    }
 
 
     return 0;
