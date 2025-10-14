99f1f2accae7fa1f68cc08852bcb7747a78fa1fa
Starwars501st <eva.briggs@ucas-edu.net>
2025-10-14 13:43:24 -0600

    Fuctions Notes

---
+++ C_programming/notes/functions_note.c
@@ -0,0 +1,19 @@
+// EB 6th Functions Notes
+#include <stdio.h>
+
+void birthday(char* name, int age){
+    printf("Happy Birthday to you\n");
+    printf("Happy Birthday to you\n");
+    printf("Happy Birthday dear %s\n", name);
+    printf("Happy Birthday to you!\n");
+    printf("Happy Birthday %s is now %d\n", name, age);
+return 0;
+}
+    int main(void){
+        birthday();
+        birthday();
+        birthday();
+
+    }
+return 0;
+
